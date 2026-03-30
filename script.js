d2eef3b662fd0248a61ee6a759269dacd7d793e2
Ruijia Li <Jennyrli1998@gmail.com>
2026-03-29 22:35:16 -0400

    Adding a temp landing page

---
+++ script.js
@@ -0,0 +1,20 @@
+// script.js
+function checkAccess() {
+    const input = document.getElementById("passwordInput");
+    const errorText = document.getElementById("errorMessage");
+    const secretPassword = "youfindme";
+  
+    if (input.value === secretPassword) {
+      window.location.href = "/main/"; 
+    } else {
+      errorText.classList.remove("hidden");
+      input.classList.add("shake");
+      
+      setTimeout(() => {
+        input.classList.remove("shake");
+      }, 500);
+  
+      input.value = "";
+      input.focus();
+    }
+  }
